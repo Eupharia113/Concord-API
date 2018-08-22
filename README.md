e7923b381d7fda7e6961d1d4f9ac5a7322fa4b2c
Eupharia113 <aria@euphmail.com>
2018-08-22 12:28:54 +0100

    Initial Project Commit
    Standard JS Style Linting, README, licence, modules and project files.

---
+++ README.md
@@ -0,0 +1,66 @@
+# ConcordAPI
+ConcordAPI is intended to be a simple way of interacting with the Concord Services and it's REST API via Node.js.
+
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
+
+**WARNING:** This module is in very early development, and has a few bugs which are known by me, you are free to create a Pull Request if any issues/bugs are fixed, please do not rely on this module solely.
+
+
+## Getting Started
+
+Install the Concord-API NPM Package with:
+```
+npm i concordapi
+```
+And require the module in your Node.js application with:
+```js
+var concord = require("concordapi");
+```
+
+
+### Setting up Concord-API
+
+The Concord API, at current time, has to have an instantiation period. This is a temporary fix to a bug which i will learn to fix soon!
+
+
+To get the API ready for usage, instantiate the API and Caching with:
+```js
+concord.connect(); //Console should print "CXDx Prices cached... Services are Ready"
+```
+
+### Reading Concord Exchange (CXDx) Prices
+
+To check a coin's price in CXD use:
+```js
+concord.price(string);
+```
+
+For example, using:
+```js
+var coin = "IC";
+setInterval(checkPrice, 5000);
+
+function checkPrice(){
+	console.log('1 '+coin+' is '+concord.price(coin)+' CXD');
+}
+```
+Would log the price of IC (Ignition Coin) every 5 seconds, due to the way caching works here, each result is one 'API-Tick' older than the requested latest price.
+
+### Reading Concord Social Platform (CSP) Profiles
+```
+Coming soon, Module bug mentioned before makes this function impossible to create.
+```
+
+## Full Examples
+  Examples in this section can be freely copy/pasted into your code and modified to your choosing.
+  ```js
+  var concord = require("concordapi");
+  concord.connect(); //Instantiate Concord Library and load cache.
+
+  var coin = "IC"; //Coin listed on CXDx to check price of, in CXD.
+  setInterval(checkPrice, 5000); //Check coin-price every 5 seconds
+
+  function checkPrice(){
+  	console.log('1 '+coin+' is '+concord.price(coin)+' CXD'); // Prints the price of the CXDx Coin
+  }
+```
---
+++ index.js
@@ -0,0 +1,68 @@
+const req = require('./node_modules/requestify')
+const basePublicUrl = 'https://www.cxd.network/api/public/v1/'
+var cachedPrices = [] /* Temporary solution until i properly learn Promises/Callbacks! Please do a pull-request if you can fix this. <3 */
+/* cachedProfiles = [] */
+
+exports.price = function (coin) {
+  if (coin && coin != null && typeof coin === 'string' && coin.length >= 2) {
+    checkPrice(coin)
+  } else {
+    console.warn('String variable missing, "' + typeof coin + ' - ' + coin + '" was recieved')
+    return
+  }
+  if (cachedPrices.length >= 1) {
+    for (var ii = 0; ii < cachedPrices.length; ii++) {
+      if (cachedPrices[ii].coin === coin.toLowerCase()) {
+        if (cachedPrices[ii].price !== undefined && cachedPrices[ii].price >= 0.00000001) {
+          /* console.log(cachedPrices[ii].price); */
+          return cachedPrices[ii].price
+        } else {
+          console.warn('API Response could not be parsed, Concord REST API may be down, or your input of "' + coin.toUpperCase() + '" is an incorrect Ticker')
+        }
+      }
+    }
+  } else {
+    return console.log('Cache has not loaded correctly, please use the connect() function to refresh cache')
+  }
+}
+exports.connect = function () { /* This exists to help with starter-caching of CXD Service API data, as i am currently unable to correctly return data from this module to a dev's main script :( */
+  try {
+    req.get(basePublicUrl + 'getprice?coin=ic').then(function (res) {
+      var price = Number(res.body)
+      if (price > 0 && price !== undefined) {
+        if (cachedPrices.length === 0) {
+          cachedPrices.push({coin: 'ic', price: price}) /* If coin isn't already cached, cache it! */
+        }
+        console.log('CXDx Prices cached... Services are Ready')
+      }
+    })
+  } catch (err) {
+    console.warn('API Request failed, error: ' + err)
+  }
+}
+
+/* None-Sandboxed functions */
+function checkPrice (coin) {
+  try {
+    req.get(basePublicUrl + 'getprice?coin=' + coin.toLowerCase()).then(function (res) {
+      var price = Number(res.body)
+      if (price >= 0.00000001 && price !== undefined) {
+        if (cachedPrices.length > 0) {
+          var i, /* eslint-disable-line */
+            iLength = cachedPrices.length
+          for (i = 0; i < iLength; i++) {
+            if (cachedPrices[i].coin === coin.toLowerCase()) {
+              cachedPrices[i].price = price /* If coin is already cached, update the cache! */
+            }
+          }
+        } else {
+          cachedPrices.push({coin: 'ic', price: price}) /* If coin isn't already cached, cache it! */
+        }
+      } else {
+        console.warn('API Response could not be parsed, Concord REST API may be down, or your input of "' + coin.toUpperCase() + '" is an incorrect Ticker')
+      }
+    })
+  } catch (err) {
+    console.warn('API Request failed, error: ' + err)
+  }
+}
